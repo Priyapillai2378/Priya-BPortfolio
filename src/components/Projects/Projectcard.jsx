cc730294af582fd4501edacd2e8896b61646de09
Priyapillai2378 <108623474+Priyapillai2378@users.noreply.github.com>
2023-11-05 17:29:18 +0530

    Update Projectcard.jsx

--- src/components/Projects/Projectcard.jsx
+++ src/components/Projects/Projectcard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./ProjectCard.module.css";
+import styles from "./Projectcard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
